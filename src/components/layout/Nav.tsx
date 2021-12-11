1008d391eb7d740b43eb618958e038c7177d3626
Parssa Kyanzadeh <parssak@gmail.com>
2021-12-11 13:28:57 -0500

    made nav fixed

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,12 +1,13 @@
-import React, { ReactElement } from 'react'
-
+import React, { ReactElement } from "react";
 
 export default function Nav(): ReactElement {
   return (
-    <div className="bg-gray-50">
-    <nav className="container flex items-center py-6">
-      <p className="text-2xl font-medium">Next.js + TailwindCSS + TypeScript</p>
+    <nav className="bg-gray-50 fixed top-0 left-0 right-0">
+      <div className="container flex items-center py-6">
+        <p className="text-2xl font-medium">
+          Next.js + TailwindCSS + TypeScript
+        </p>
+      </div>
     </nav>
-    </div>
-  )
+  );
 }
