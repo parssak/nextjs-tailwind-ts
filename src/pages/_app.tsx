c29f1f641a63718d5ed386c7824bf6280ad22863
Parssa Kyanzadeh <parssak@gmail.com>
2021-12-18 12:30:42 -0500

    removed excess components, updated tailwind version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import 'styles/index.css'
-import Footer from 'components/layout/Footer'
-import Nav from 'components/layout/Nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,9 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>NextJS TW</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Nav />
       <Component {...pageProps} />
-      <Footer />
     </>
   )
 }
