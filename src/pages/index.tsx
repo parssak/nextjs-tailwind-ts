ce211ee86479234454ee71fcdd361af5bfc976f3
parssak <parssak@gmail.com>
2022-11-27 15:01:24 -0500

    feat: cleaned up landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 export default function Home() {
   return (
     <>
-      <main className="container min-h-screen grid pt-48 md:pt-0 md:place-items-center">
-        <div className="md:text-center">
-          <h1 className="text-5xl font-semibold">
-            Next.js, TailwindCSS, TypeScript Template!
-          </h1>
-          <p className="text-2xl text-gray-600 md:max-w-2xl mx-auto mt-12 md:mt-6">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda,
-            architecto, ducimus cumque temporibus optio quae dolore expedita
-            itaque ut minima labore nisi veniam amet rerum quidem, quam dolorum
-            vero nulla.
-          </p>
-        </div>
+      <main className="container min-h-screen pt-48 md:pt-24">
+        <h1 className="text-3xl tracking-tight font-semibold">Next.js, TailwindCSS, TypeScript Template</h1>
+        <p className='mt-4'>
+          Build something amazing with this template.
+        </p>
       </main>
     </>
   );
