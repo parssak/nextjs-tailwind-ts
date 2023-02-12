d178c36c7c2c340193b6127367a0d1aac4a522a2
Parssa Kyanzadeh <33424333+parssak@users.noreply.github.com>
2023-02-11 23:14:32 -0500

    fixed hierarchy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,35 @@ export default function Page() {
       <Text variant="h1">A Next 13 App with TailwindCSS 3.2</Text>
       <Text className="mt-size-4y">
         Also, pre-configured with{" "}
-        <Text as="a" href={"https://universal-ui.vercel.app/"} variant="code" className="font-mono" size='sm'>
-          @parssa/universal-ui
-        </Text>
-        <Button
-          target={"_blank"}
+        <Text
           as="a"
           href={"https://universal-ui.vercel.app/"}
-          className="mt-size-4y group flex w-max"
-          trailingIcon={
-            <svg
-              className="w-full h-full group-hover:translate-x-1 transition-transform"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-            </svg>
-          }
+          variant="code"
+          className="font-mono"
+          size="sm"
         >
-          Learn More
-        </Button>
+          @parssa/universal-ui
+        </Text>
       </Text>
+      <Button
+        target={"_blank"}
+        as="a"
+        href={"https://universal-ui.vercel.app/"}
+        className="mt-size-4y group flex w-max"
+        trailingIcon={
+          <svg
+            className="w-full h-full group-hover:translate-x-1 transition-transform"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg>
+        }
+      >
+        Learn More
+      </Button>
     </div>
   );
 }
